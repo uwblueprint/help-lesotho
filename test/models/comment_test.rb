85ae1a2c2e10845876bfb689cd670ab7907663b2
rysharm <rsharman@uwaterloo.ca>
2017-11-04 16:26:11 -0400

    Fix indentation

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,12 +4,12 @@ class CommentTest < ActiveSupport::TestCase
 
   test "comment must have a user" do
     comment = new_comment(user: nil)
-  assert_not comment.save 
+    assert_not comment.save 
   end
 
   test "comment must belong to a post" do
     comment = new_comment(post: nil)
-  assert_not comment.save 
+    assert_not comment.save 
   end
 
 end
